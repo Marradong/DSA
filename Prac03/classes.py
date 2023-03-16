4001dd7acc8d8a1bcff4b763cce6e9cf73ecf433
marradong <battet42@gmail.com>
2023-03-16 15:39:52 +0800

    Question 1 partial complete

--- Prac03/classes.py
+++ Prac03/classes.py
@@ -4,6 +4,70 @@ class DSAStack():
 
     def __init__(self, capacity=100):
         self.capacity = capacity
-        self.stack
-        self.count
+        self.stack = np.zeros(capacity)
+        self.count = 0
+    
+
+    def getCount(self):
+        return self.count
+    
+
+    def isEmpty(self):
+        if self.count == 0:
+            empty = True
+        else:
+            empty = False
+        return empty
+
+
+    def isFull(self):
+        if self.count == self.capacity:
+            full = True
+        else:
+            full = False
+        return full
+    
+
+    def push(self, value):
+        if not self.isFull():
+            self.stack[self.count] == value
+            self.count = self.count + 1
+    
+
+    def top(self):
+        if not self.isEmpty():
+            topVal = self.stack[self.count]
+            return topVal
         
+
+    def pop(self):
+        topVal = self.top()
+        self.count = self.count - 1
+        return topVal
+
+
+class DSAQueue():
+
+    def __init__(self, capacity=100):
+        self.capacity = capacity
+        self.queue = np.zeros(self.capacity)
+        self.count = 0
+
+    def getCount(self):
+        return self.count
+    
+
+    def isEmpty(self):
+        if self.count == 0:
+            empty = True
+        else:
+            empty = False
+        return empty
+
+
+    def isFull(self):
+        if self.count == self.capacity:
+            full = True
+        else:
+            full = False
+        return full
